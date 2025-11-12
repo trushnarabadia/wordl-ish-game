b774ac04c622f7c5c6b4e7f28f3ffd8b0938e976
Riana Rabadia <riann@Rianas-MBP.communityfibre.co.uk>
2025-11-12 17:44:56 +0000

    all criteria for word_validator complete

--- game/word_validator.py
+++ game/word_validator.py
@@ -1,6 +1,6 @@
 class WordValidator:
     def is_valid_word(self, word):
-        if len(word) == 4:
+        if len(word) == 4 and word.isalpha():
             return True
         else:
             return False
--- tests/test_word_validator.py
+++ tests/test_word_validator.py
@@ -11,3 +11,15 @@ def test_rejects_words_too_short():
 def test_rejects_words_too_long():
     validator = WordValidator()
     assert validator.is_valid_word("hello") == False
+
+def test_rejects_word_if_contains_numbers():
+    validator = WordValidator()
+    assert validator.is_valid_word("tes3") == False
+
+def test_rejects_word_if_contains_special_characters():
+    validator = WordValidator()
+    assert validator.is_valid_word("te$t") == False
+
+def test_rejects_empty_string():
+    validator = WordValidator()
+    assert validator.is_valid_word("") == False
