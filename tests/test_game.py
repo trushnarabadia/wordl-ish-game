67b6a5fc02c4271ec774c504882f4f3466939392
Riana Rabadia <riann@Rianas-MBP.communityfibre.co.uk>
2025-11-19 15:33:12 +0000

    created check_guess to verify how many correct letters there are

--- tests/test_game.py
+++ tests/test_game.py
@@ -14,3 +14,15 @@ def test_accepts_valid_secret_word():
     game = Game()
     result = game.set_secret_word("word")
     assert result == True
+
+def test_check_guess_returns_number_of_correct_letters():
+    game = Game()
+    game.set_secret_word("game")
+    result = game.check_guess("gale")
+    assert result == 3  # 'g', 'a', and 'e' are
+
+def test_check_guess_no_correct_letters():
+    game = Game()
+    game.set_secret_word("test")
+    result = game.check_guess("abcd")
+    assert result == False
